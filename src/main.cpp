88985ce424e3ca666bc4aef0bea7e9de4308fd04
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-10-17 17:01:23 -0400

    update file structure to work with platformio

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include <string.h>
 using namespace std;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   int sensorValue = analogRead(A0);
   Serial.println(sensorValue);
-  
+
   // double voltage = sensorValue * (1024/5);
   // Serial.println("Voltage: " + (voltage);
   delay(500);
