d603dcce614bf619a7e1bbacdf288752213598cf
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-10-17 16:25:59 -0400

    add basic sensor test code, platformio project, component list

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sd-team17-2023
 Senior Design Team 17, 2022-2023.
+
+Members: 
+1. Nate Anderson
+2. Jordan Alexander
+3. Ben DeWeerd
+4. Anna Giboney
---
+++ free2breathe/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <string.h>
+using namespace std;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  
+  // double voltage = sensorValue * (1024/5);
+  // Serial.println("Voltage: " + (voltage);
+  delay(500);
+}
