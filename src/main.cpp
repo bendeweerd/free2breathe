d522a20923108893616d3372a144cd1edb98bb24
Ben DeWeerd <bendeweerd2001@gmail.com>
2023-01-16 09:37:37 -0500

    add basic code for additional sensors

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include "leds/LEDController.h"
 #include "sensors/COsensor.h"
+#include "sensors/NO2Sensor.h"
+#include "sensors/O3sensor.h"
+#include "sensors/SO2sensor.h"
 
 #define NUM_LEDS 135
 #define LED_DATA_PIN 8
@@ -10,9 +13,15 @@
 CRGB leds[NUM_LEDS];
 
 f2b::LEDController LEDController = f2b::LEDController(leds, NUM_LEDS);
-COSensor sensor0(A0, A1);
+COSensor coSensor(A0, A1);
+O3Sensor o3Sensor(A2, A3);
+SO2Sensor so2Sensor(A4, A5);
+NO2Sensor no2Sensor(A6, A7);
 
 float coPPM = 0;
+float o3PPM = 0;
+float so2PPM = 0;
+float no2PPM = 0;
 
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
@@ -31,15 +40,18 @@ void setup() {
 }
 
 void loop() {
-  coPPM = sensor0.read();
+  coPPM = coSensor.read();
+  o3PPM = o3Sensor.read();
+  so2PPM = so2Sensor.read();
+  no2PPM = no2Sensor.read();
 
-  if (coPPM > 10) {
-    LEDController.currentLEDState = f2b::LEDState::PULSE_RED;
-  } else if (coPPM > 5) {
-    LEDController.currentLEDState = f2b::LEDState::PULSE_YELLOW;
-  } else {
-    LEDController.currentLEDState = f2b::LEDState::LOADING_SPIN;
-  }
+  // if (coPPM > 10) {
+  //   LEDController.currentLEDState = f2b::LEDState::PULSE_RED;
+  // } else if (coPPM > 5) {
+  //   LEDController.currentLEDState = f2b::LEDState::PULSE_YELLOW;
+  // } else {
+  //   LEDController.currentLEDState = f2b::LEDState::LOADING_SPIN;
+  // }
 
   LEDController.UpdateLEDs();
 }
